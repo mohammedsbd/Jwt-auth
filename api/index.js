c83e256e7eada88d4fd2c421abc165ec42aadc5c
mohammedsbd <mohammed1112ok@gmail.com>
2025-06-19 10:05:19 -0700

    parse json

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 // create an express app
 const app = express();
+app.use(express.json()); // to parse JSON bodies
 
 const user=[
     {
@@ -10,9 +11,21 @@ const user=[
         password: 'jhon0908',
         isAdmin: true,
       
+    },
+
+    {
+        id:2,
+        username: 'Jhane Doe',
+        password: 'jhane0908',
+        isAdmin: false,
+      
     }
 ]
-
+//post to api/login
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+   
+});
 // listen
 const port = 5050;
 app.listen(port, () => {
