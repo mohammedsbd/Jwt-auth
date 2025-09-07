a38e641ff240ad6a3b521cf253b990cb7ded8c30
mohammedsbd <mohammed1112ok@gmail.com>
2025-09-07 11:42:36 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6862,3 +6862,47 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 });
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
+const port = 5050;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
+            isAdmin: user.isAdmin,
+           accessToken
+       });
+    }
+    else{
+        res.status(401).json({message: 'Invalid username or password'});
+    }
+   
+});
+// listen
+const port = 5050;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
+if(user){
+       //generate access token
+       const accessToken=jwt.sign({id:user.id, isAdmin:user.isAdmin}, "mysecretkey")
+       res.json({
+          username: user.username,
+            isAdmin: user.isAdmin,
+           accessToken
+       });
+    }
+    else{
+        res.status(401).json({message: 'Invalid username or password'});
+    }
+   
+});
+// listen
+const port = 5050;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
