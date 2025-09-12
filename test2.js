b8054ce4293ac2a5955dcc01e66e6c524d50a2cf
mohammedsbd <mohammed1112ok@gmail.com>
2025-09-12 06:58:00 -0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -23014,3 +23014,55 @@ app.listen(port, () => {
 });
 });
 });
+});
+// listen
+const port = 5050;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
+});
+    else{
+        res.status(401).json({message: 'Invalid username or password'});
+    }
+   
+});
+// listen
+const port = 5050;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
+});
+    else{
+        res.status(401).json({message: 'Invalid username or password'});
+    }
+   
+});
+// listen
+const port = 5050;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
+if(user){
+       //generate access token
+       const accessToken=jwt.sign({id:user.id, isAdmin:user.isAdmin}, "mysecretkey")
+       res.json({
+          username: user.username,
+            isAdmin: user.isAdmin,
+           accessToken
+       });
+    }
+    else{
+        res.status(401).json({message: 'Invalid username or password'});
+    }
+   
+});
+// listen
+const port = 5050;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+});
+});
